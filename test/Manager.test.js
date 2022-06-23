9e7e740a5455660e64de61433da8ab274e236fde
gofoward-z <zlepeng.ucf@gmail.com>
2022-06-23 17:06:46 -0400

    add manager test

---
+++ test/Manager.test.js
@@ -0,0 +1,11 @@
+test("Can set office number via constructor argument", () => {
+    console.log("Can set office number via constructor argument");
+});
+
+test('getRole() should return "Manager"', () => {
+    console.log("Can set office number via constructor argument");
+});
+
+test("can get office number via getOffice()", () => {
+    console.log("Can set office number via constructor argument");
+});

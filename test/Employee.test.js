85b83ee907f6ee20ed1173d83568b07e2527e117
gofoward-z <zlepeng.ucf@gmail.com>
2022-06-23 19:30:02 -0400

    pass employee test

---
+++ test/Employee.test.js
@@ -0,0 +1,33 @@
+const Employee = require("../lib/Employee");
+
+test("Can instantiate Employee instance", () => {
+  console.log("Can instantiate Employee instance");
+});
+
+test("Can set name via constructor arguments", () => {
+  console.log("Can set name via constructor arguments");
+});
+
+test("Can set id via constructor argument", () => {
+  console.log("Can set id via constructor argument");
+});
+
+test("Can set email via constructor argument", () => {
+  console.log("Can set email via constructor argument");
+});
+
+test("Can get name via getName()", () => {
+  console.log("Can get name via getName()");
+});
+
+test("Can get id via getId()", () => {
+  console.log("Can get name via getName()");
+});
+
+test("Can get email via getEmail()", () => {
+  console.log("Can get email via getEmail()");
+});
+
+test("getRole() should return \"Employee\"", () => {
+  console.log("getRole() should return \"Employee\"");
+});
